b9dd34dfdfe3e1ac2b0fee7b175410b558c7948e
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-13 12:28:01 +0700

    6. Home Page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,11 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>[header]</header>
+        <main>{children}</main>
+        <footer>[footer]</footer>
+      </body>
     </html>
   );
 }
