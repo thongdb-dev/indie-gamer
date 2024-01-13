acd7e6f8c7fd61f7ba18fa65cf1c267eb0f6fcbc
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-13 19:17:20 +0700

    20. Installing Tailwind CSS

--- app/reviews/layout.tsx
+++ app/reviews/layout.tsx
@@ -1,7 +1,7 @@
 export default function ReviewsLayout({ children }) {
   return (
     <div style={{ display: "flex" }}>
-      <div style={{ border: "solid red 1px" }}>[reviews menu bar]</div>
+      {/* <div style={{ border: "solid red 1px" }}>[reviews menu bar]</div> */}
       <div>{children}</div>
     </div>
   );
