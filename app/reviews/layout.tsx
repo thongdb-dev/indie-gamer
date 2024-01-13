c920f6d0fae079b8eda8768a9ca9805ab06addb0
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-13 13:35:30 +0700

    11. Nested Layouts

---
+++ app/reviews/layout.tsx
@@ -0,0 +1,8 @@
+export default function ReviewsLayout({ children }) {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ border: "solid red 1px" }}>[reviews menu bar]</div>
+      <div>{children}</div>
+    </div>
+  );
+}
