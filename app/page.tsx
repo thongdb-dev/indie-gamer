41183f08c17a6745af8195fc4bc64c62f6b5ea6e
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-13 19:50:35 +0700

    23. Headinng Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
+import Heading from "../components/Heading";
 
 export default function HomePage() {
-  // useEffect(() => {
-  //   window.alert("Welcome to my site!");
-  // }, []);
-
   return (
     <>
-      <h1>Indie Gamer</h1>
+      <Heading>Indie Gamer</Heading>
       <p>Only the best indie games, reviewed for you.</p>
     </>
   );
