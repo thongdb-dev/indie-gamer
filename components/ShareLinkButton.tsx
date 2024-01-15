7b56d9ce8ffd291b8dc4d3a5c5a992be61d75617
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-15 22:32:35 +0700

    47. Component State

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import { useState } from "react";
+
 export default function ShareLinkButton() {
+  const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
-    console.log("Clicked!");
+    navigator.clipboard.writeText(window.location.href);
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 1500);
   };
 
   return (
@@ -10,7 +18,7 @@ export default function ShareLinkButton() {
       className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
       onClick={handleClick}
     >
-      Share Link
+      {clicked ? "Link Copied" : "Share Link"}
     </button>
   );
 }
