4b54b1e961a016c0221fe0b3aca4d672079a1061
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-15 22:46:06 +0700

    49. Heroicons

--- components/ShareLinkButton.tsx
+++ components/ShareLinkButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { LinkIcon } from "@heroicons/react/20/solid";
 
 export default function ShareLinkButton() {
   const [clicked, setClicked] = useState(false);
@@ -15,9 +16,10 @@ export default function ShareLinkButton() {
 
   return (
     <button
-      className="border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
+      className="border flex gap-1 items-center px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700"
       onClick={handleClick}
     >
+      <LinkIcon className="h-4 w-4" />
       {clicked ? "Link Copied" : "Share Link"}
     </button>
   );
