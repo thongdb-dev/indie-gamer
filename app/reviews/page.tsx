11839ccd42162fe2c78d25e2ccac8cd5b85bfa84
Đinh Bách Thông <thongdb.dev@gmail.com>
2024-01-15 16:56:27 +0700

    38. Rendering Arrays

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -5,40 +5,26 @@ import { getReviews } from "@/lib/reviews";
 export default async function ReviewsPage() {
   const reviews = await getReviews();
 
-  console.log("reviews: ", reviews);
-
   return (
     <>
       <Heading>Reviews</Heading>
-      <ul className="flex flex-col gap-3">
-        <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
-          <Link href={"/reviews/hollow-knight"}>
-            <img
-              src="/images/hollow-knight.jpg"
-              alt=""
-              width={320}
-              height={180}
-              className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
-        <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
-          <Link href={"/reviews/stardew-valley"}>
-            <img
-              src="/images/stardew-valley.jpg"
-              alt=""
-              width={320}
-              height={180}
-              className="rounded-t"
-            />
-            <h2 className="font-orbitron font-semibold py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews?.map((review) => (
+          <li className="border w-80 bg-white rounded shadow hover:shadow-xl">
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt=""
+                width={320}
+                height={180}
+                className="rounded-t"
+              />
+              <h2 className="font-orbitron font-semibold py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
